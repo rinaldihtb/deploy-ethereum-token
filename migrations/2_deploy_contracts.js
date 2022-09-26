88fdb9993018c1a091db70aa74a2063936cd9094
rinaldihtb <rinaldihtb@ymail.com>
2022-09-26 10:05:41 +0700

    set token to be 1 million

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Token = artifacts.require("MyToken");
-const initialSupply = 10000;
+const initialSupply = 1000000;
 
 module.exports = function(deployer, network, account) {
     deployer.deploy(Token, initialSupply)
